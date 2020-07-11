15c95f2df0384eec5049177d3cd3b7d44a1acf1e
Barush Mendez <barush_mendez-3.0@hotmail.com>
2020-07-10 21:17:46 -0500

    Primer commit

---
+++ faces.py
@@ -0,0 +1,54 @@
+import numpy as numpy
+import cv2
+import pickle
+
+
+face_cascade = cv2.CascadeClassifier('cascades/haarcascade_frontalface_alt2.xml')
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainer.yml")
+labels = {}
+with open("labels.pickle", 'rb') as f:
+    og_labels = pickle.load(f)
+    labels = {v:k for k,v in og_labels.items()}
+
+cap = cv2.VideoCapture(0)
+
+while(True):
+    # Capture frame by frame
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.5, minNeighbors=5)
+    for (x,y,w,h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+
+        id_, conf = recognizer.predict(roi_gray)
+        if conf >= 45 and conf <= 85:
+            print(id_)
+            print(labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            name = labels[id_]
+            color = (255,255,255)
+            stroke = 2
+            cv2.putText(frame, name, (x,y), font, 1, color, stroke, cv2.LINE_AA)
+
+        img_item = "my-image.png"
+        cv2.imwrite(img_item, roi_gray)
+
+        color = (255, 0, 0)
+        stroke = 2
+        width = x + w
+        height = y + h
+        cv2.rectangle(frame, (x,y), (width, height), color, stroke)
+        
+
+    # Display the resulting frame   
+    resized = cv2.resize(frame, (960, 540));
+    cv2.imshow('frame', resized)
+    if (cv2.waitKey(20) & 0xFF == ord('q')):
+        break
+
+# When everything is done, release the capture
+cap.release()
+cv2.destroyAllWindows()
